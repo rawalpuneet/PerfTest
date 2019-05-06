7b7a9e336c4e2db4729cb4b4946050a24cbab1a0
Puneet Rawal <rawalpuneet@gmail.com>
2019-05-06 10:53:48 -0700

    some more changes to req.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const marklogic = require('marklogic');
+const my = require('./my-connection.js');
+
+const db = marklogic.createDatabaseClient(my.connInfo);
+const qb = marklogic.queryBuilder;
+console.time('simple search');
+// First search sets the timestamp value
+db.documents.query(
+  qb.where(
+	  qb.and(
+	  	//qb.range('StartDate', '>=', '1900-01-01')
+		  //,
+		//qb.value('RequestType','GRI*'),
+		//qb.not(  qb.value('ErrorMessage',['Wes*', 'Other Messages']) )  
+      )
+	  
+  
+  )
+ // .calculate( qb.facet('collection', qb.collection()) ) 
+//  .calculate(qb.facet('RequestType'))
+  .withOptions({search:['unfiltered'],  categories: ['content']}  )
+   .slice(0,50000)
+
+).result().then( function(results) {
+	
+    //console.log(JSON.stringify(results, null, 2));
+    console.timeEnd('simple search');
+  process.exit()	
+});
+
+
+
+
---
+++ my-connection.js
@@ -0,0 +1,9 @@
+module.exports = {
+  connInfo: {
+    host: 'localhost',
+    port: 8064,
+    user: 'admin',
+    password: 'admin',
+	database: 'hfs-sparkii-content'
+  }
+};
